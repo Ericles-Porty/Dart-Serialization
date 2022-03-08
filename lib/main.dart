78f353c85b2d047bfc19048e37d0115d4d612dae
Ericles-Porty <ericlesdsantos@gmail.com>
2022-03-08 00:17:10 -0300

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:dio/dio.dart';
 import 'models/Comentarios.dart';
 
 Future<List<Comentarios>> getHttp() async {
-    var response = await Dio().get('https://jsonplaceholder.typicode.com/comments');
-    List<dynamic> datab = response.data;
-    List<Comentarios> comentarios = datab.map((x) => Comentarios.fromJson(x)).toList();
-    return comentarios;
-  }
+  var response = await Dio().get('https://jsonplaceholder.typicode.com/comments');
+  List<dynamic> datab = response.data;
+  List<Comentarios> comentarios = datab.map((x) => Comentarios.fromJson(x)).toList();
+  return comentarios;
+}
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Consumir API',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -46,12 +47,78 @@ class _HomeState extends State<Home> {
             return const Center(child: CircularProgressIndicator());
           }
           List<Comentarios> listaComentarios = snapshot.data as List<Comentarios>;
-          return ListView.separated(
-            itemCount: listaComentarios.length,
-            itemBuilder: (context, index) {
-              return Text(listaComentarios[index].toJson().toString());
-            },
-            separatorBuilder: (context, index) => const SizedBox(height: 10),
+          return Column(
+            children: [
+              Container(
+                height: 100,
+                color: Colors.blue,
+                margin: const EdgeInsets.all(5),
+                child: const Center(
+                  child: Text(
+                    'Comentarios',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
+                  scrollDirection: Axis.vertical,
+                  itemCount: listaComentarios.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      child: RichText(
+                        text: TextSpan(
+                          children: <TextSpan>[
+                            const TextSpan(
+                              text: 'Nome: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: listaComentarios[index].name + '\n',
+                            ),
+                            const TextSpan(
+                              text: 'E-mail: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: listaComentarios[index].email + '\n',
+                            ),
+                            const TextSpan(
+                              text: 'Comentario: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: listaComentarios[index].body + '\n',
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) => const SizedBox(height: 20),
+                ),
+              ),
+            ],
           );
         },
       ),
